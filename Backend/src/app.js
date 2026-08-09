58d08d949f479474f2b6414fb2bd31da6fcb9fda
Manish Kumar <manishjha130702@gmail.com>
2026-08-09 22:19:53 +0530

    updated code

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://interview-gen-ai-project-alpha.vercel.app/",
     credentials: true
 }))
 
